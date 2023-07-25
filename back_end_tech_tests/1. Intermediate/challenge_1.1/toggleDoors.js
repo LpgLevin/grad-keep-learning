1e0a658ec515e8dea3d95df885e07eaa532e9bfb
LpgLevin <lpglevin@gmail.com>
2023-07-25 12:19:35 +0100

    toggleDoors translate instructions

--- back_end_tech_tests/1. Intermediate/challenge_1.1/toggleDoors.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.1/toggleDoors.js
@@ -1,3 +1,17 @@
 const toggleDoors = () => {}
 
+// create empty array to hold doors
+
+// create loop ot iterate from 1 to 'n' (the number passed in), pushing a one into the doors array for each iteration
+
+// iterate through the dorrs array closing every second door (changing it to a zero)
+
+//then every 3rd door
+
+//then every 4th
+
+// until you get to every nth door (so you only close the final door) by changing it to zero
+
+// return the indexes of all the remaining open doors (1s)
+
 module.exports = toggleDoors;
